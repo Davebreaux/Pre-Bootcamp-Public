6474223f570eae4f9629910f0c0527f5e7d31424
David Breaux <74641618+Davebreaux@users.noreply.github.com>
2021-04-07 15:35:16 -0500

    JSPredict

---
+++ Breaux_David_JSPredict.js
@@ -0,0 +1,10 @@
+// Predict 1: Console will print "I was born in 1980" (no quotes)
+
+// Predict 2: Console will print "I was born in 1980" (no quotes)
+
+/* Predict 3: Console will print 4 lines.  
+    Summing Numbers!
+    num1 is: 10
+    num2 is: 20
+    30
+    */
